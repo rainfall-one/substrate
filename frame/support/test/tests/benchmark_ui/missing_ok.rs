1715c0d116ccf73defdec3c03696d1519302e62f
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-03-02 18:08:53 +0100

    Add Positive and Negative UI test
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

---
+++ frame/support/test/tests/benchmark_ui/missing_ok.rs
@@ -0,0 +1,19 @@
+use frame_benchmarking::v2::*;
+use frame_support_test::Config;
+
+#[benchmarks]
+mod benches {
+	use super::*;
+
+	#[benchmark]
+	fn bench() -> Result<(), BenchmarkError> {
+		let a = 2 + 2;
+
+		#[block]
+		{}
+		assert_eq!(a, 4);
+		// No Ok(())
+	}
+}
+
+fn main() {}
